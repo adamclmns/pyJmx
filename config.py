e4244291d16ef4e85606ff25aed2e9a352932323
AdamClmns <adam@AdamClmns.com>
2016-03-20 16:27:34 -0500

    Initial Commit - proof of concept
    Code-complete proof of concept using python2.7 standard libraries,
    specifically subprocess. to execute shell commands.

---
+++ config.py
@@ -0,0 +1,12 @@
+# Adam Clemons
+# 03-18-2016
+# config.py - constants meant to be used by the other scripts.
+
+import os
+thisDir = os.getcwd()
+jMeterPath="jmeter\\bin\\"
+jmxFolderPath="jmeter\\bin\\tests\\"
+
+
+
+setPathCommand = "SET PATH=%PATH%;"+thisDir+jMeterPath
